73a979f2ef6f75374a2c1ba9179d40668feeb017
ChengWenLee <ChanWenLee@gmail.com>
2023-03-12 20:26:10 +0800

    Initial Commit

---
+++ Health_Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Health_Food
+//
+//  Created by Lee chanwen on 3/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
